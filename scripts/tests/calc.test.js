bf98c6c635b192f7baa1bdab43ee071e804103e8
Mihaly Lovrencsics <lomysa@gmail.com>
2023-08-29 22:31:45 +0000

    Add more test to experiment with

--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -5,6 +5,9 @@ describe("Calculator", () => {
         test("should return 42 for 20 + 22", () => {
             expect(addition(20 ,22)).toBe(42);
         })
+        test("should return 50  for 25 + 25", () => {
+            expect(addition(25, 25)).toBe(50);
+        })
     });
     describe("Subtraction function", () => {
         
